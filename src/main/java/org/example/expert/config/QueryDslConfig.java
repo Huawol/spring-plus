1f8beea513228219480d0f08abd60bc02225e199
taehyeon <max5915@naver.com>
2025-06-27 16:42:53 +0900

    Lv 2 : 8

---
+++ src/main/java/org/example/expert/config/QueryDslConfig.java
@@ -0,0 +1,21 @@
+package org.example.expert.config;
+
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
+// querydsl 사용을 위한 사전준비
+@Configuration
+@RequiredArgsConstructor
+public class QueryDslConfig {
+
+    private final EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
+    }
+}
---
+++ src/test/java/org/example/expert/domain/todo/repository/TodoRepositoryTest.java
@@ -0,0 +1,52 @@
+package org.example.expert.domain.todo.repository;
+
+import org.example.expert.config.QueryDslConfig;
+import org.example.expert.domain.todo.entity.Todo;
+import org.example.expert.domain.user.entity.User;
+import org.example.expert.domain.user.enums.UserRole;
+import org.example.expert.domain.user.repository.UserRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+@DataJpaTest
+@DisplayName("Repository:Todo")
+@ActiveProfiles("test")
+@Import(QueryDslConfig.class)
+class TodoRepositoryTest {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private TodoRepository todoRepository;
+
+    private User user;
+
+    @BeforeEach
+    void setUp() {
+        user = new User("hong-gd@gmail.com", "nickname","password", UserRole.USER);
+        userRepository.save(user);
+
+        Todo todo = new Todo("todo title", "todo contents", "sunny", user);
+        todoRepository.save(todo);
+    }
+
+    @Test
+    @DisplayName("QueryDsl로 전환한 findByIdWithUser의 동작 검증")
+    void findByIdWithUser() {
+        // Given
+        Long userId = user.getId();
+
+        // When
+        Todo todo = todoRepository.findByIdWithUser(userId).orElseThrow();
+
+        // Then
+        assertThat(todo).isNotNull();
+    }
+}
